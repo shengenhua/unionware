1873cba55fdc8be7e37b5caf44b0134570dd55fd
上帝zz <2640340518@qq.com>
2025-05-08 10:14:11 +0800

    提交 1.0.4 修改初始化

--- baselib/src/main/java/com/unionware/basicui/app/AppInitializer.kt
+++ baselib/src/main/java/com/unionware/basicui/app/AppInitializer.kt
@@ -7,7 +7,7 @@ import androidx.startup.Initializer
 class AppInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        UnionwareApp.init(context as Application)
+//        UnionwareApp.init(context as Application)
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
